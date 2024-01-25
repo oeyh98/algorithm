a2e1b57c343b6f408a5e493314972c7dfe0b8810
jinhyeok <skusus123@gmail.com>
2024-01-25 20:28:58 +0900

    [Silver III] Title: N과 M (4), Time: 60 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,15 @@
+n, m = map(int, input().split())
+result = []
+def back():
+    if len(result) == m:
+        print(*result, end=' ')
+        print()
+        return
+
+    for i in range(1, n+1):
+        if len(result) == 0 or (result and result[-1] <= i):
+            result.append(i)
+            back()
+            result.pop()
+
+back()
