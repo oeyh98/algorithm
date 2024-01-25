3970c4315df81f04bf73fa38dc7b85fe5fffb9ab
jinhyeok <skusus123@gmail.com>
2024-01-25 18:06:04 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 52 ms, Memory: 40556 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -0,0 +1,18 @@
+def star(n):
+    if n == 3:
+        return ['***', "* *", "***"]
+
+    arr = star(n//3)
+    stars = []
+
+    for i in arr:
+        stars.append(i*3)
+    for i in arr:
+        stars.append(i + ' '*(n//3) + i)
+    for i in arr:
+        stars.append(i * 3)
+
+    return stars
+
+n = int(input())
+print('\n'.join(star(n)))
