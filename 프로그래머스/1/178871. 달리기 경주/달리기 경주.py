be169206ea748886be47ef24480a5029e3b09cdc
jinhyeok <skusus123@gmail.com>
2024-03-31 17:46:27 +0900

    [level 1] Title: 달리기 경주, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
@@ -0,0 +1,9 @@
+def solution(players, callings):
+    dic = {player:i for i, player in enumerate(players)}
+    for call in callings:
+        idx = dic[call]
+        dic[call] -= 1
+        dic[players[idx-1]] += 1
+        players[idx-1], players[idx] = players[idx], players[idx-1]
+
+    return players
