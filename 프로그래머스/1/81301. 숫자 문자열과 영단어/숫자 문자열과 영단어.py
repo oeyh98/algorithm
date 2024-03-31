4b07adc6997868ba7b3ae5ee43c9782f51a2666a
jinhyeok <skusus123@gmail.com>
2024-03-31 18:42:05 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,9 @@
+def solution(s):
+    answer = ''
+    word = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    
+    for i in range(len(word)):
+        s = s.replace(word[i], str(i))
+    
+    
+    return int(s)
