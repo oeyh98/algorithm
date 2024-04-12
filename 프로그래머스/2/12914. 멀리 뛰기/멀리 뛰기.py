e8961b45a82f48c18a747253c11ee4cdf7c3272d
jinhyeok <skusus123@gmail.com>
2024-04-12 10:38:07 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.64 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,10 +1,8 @@
 def solution(n):
     dp = [1] * (n + 1)
     
-    if n < 2:
-        return dp[n]
-    else:
-        for i in range(2, n+1):
-            dp[i] = dp[i-1] + dp[i-2]
-        
-        return dp[n] % 1234567
+
+    for i in range(2, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+
+    return dp[n] % 1234567
