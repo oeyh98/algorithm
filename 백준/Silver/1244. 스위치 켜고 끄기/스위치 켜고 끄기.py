f78c8132f6870e45a0f3f08c23ddca93955f23ab
jinhyeok <skusus123@gmail.com>
2024-05-08 15:54:46 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
@@ -1,10 +1,9 @@
 switch_len = int(input())
 switch = (list(map(int, input().split())))
-student_len = int(input())
-student = [list(map(int, input().split())) for _ in range(student_len)]
+student = int(input())
 
-for s in student:
-    gender, number = s[0], s[1]
+for s in range(student):
+    gender, number = map(int, input().split())
 
     if gender == 1:
         i = 1
