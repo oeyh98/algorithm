6c00dcf294bf53191231ee59c98586cf5ee3fbf9
jinhyeok <skusus123@gmail.com>
2024-05-31 16:09:06 +0900

    [Silver I] Title: 데스 나이트, Time: 72 ms, Memory: 34068 KB -BaekjoonHub

--- 백준/Silver/16948. 데스 나이트/데스 나이트.py
+++ 백준/Silver/16948. 데스 나이트/데스 나이트.py
@@ -1,21 +1,37 @@
 from collections import deque
 
-def bfs(y, x):
-    q = deque()
-    q.append((y, x))
-    graph[y][x] = 0
-    while q:
-        y, x = q.popleft()
-        for dy, dx in d:
-            ny, nx = y+dy, x+dx
-            if 0 <= ny < n and 0 <= nx < n and graph[ny][nx] == -1:
-                q.append((ny, nx))
-                graph[ny][nx] = graph[y][x]+1
-
 n = int(input())
-r1, c1, r2, c2 = map(int, input().split())
-graph = [[-1] * n for _ in range(n)]
-d = [(-2, -1), (-2, 1), (0, -2), (0, 2), (2, -1), (2, 1)]
-bfs(r1, c1)
+graph = [[0]*n for _ in range(n)]
 
-print(graph[r2][c2])
+dx = [-2, -2, 0, 0, 2, 2]
+dy = [-1, +1, -2, +2, -1, +1]
+
+r1, c1, r2, c2 = map(int, input().split())
+
+
+def bfs(x, y):
+    dq = deque()
+    dq.append((x, y))
+    graph[x][y] = 0
+
+    while dq:
+        x, y = dq.popleft()
+
+        if x == r2 and y == c2:
+            return graph[x][y]
+
+        for i in range(6):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < n:
+                if graph[nx][ny] == 0:
+                    graph[nx][ny] = graph[x][y] + 1
+                    dq.append((nx, ny))
+
+
+result = bfs(r1, c1)
+if result:
+    print(result)
+else:
+    print(-1)
