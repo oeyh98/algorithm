cd2db2d7a0e9a77a3d4f0ce66cdccf27b6f36213
jinhyeok <skusus123@gmail.com>
2024-06-01 17:18:40 +0900

    [Silver IV] Title: 스위치와 램프, Time: 120 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/16960. 스위치와 램프/스위치와 램프.py
+++ 백준/Silver/16960. 스위치와 램프/스위치와 램프.py
@@ -1,22 +1,26 @@
-N, M = map(int, input().split())  
-lamp = [0] * (M+1)  
-switch = [[]]  
-lamp[0] = 1  
-for i in range(N):  
+n, m = map(int, input().split())
+lamp = [0] * (m+1)
+lamp[0] = 1
+switch = []
+
+for _ in range(n):
     order = list(map(int, input().split()))[1:]
     switch.append(order)
-    for j in order:
-        lamp[j] += 1  
 
-if 0 in lamp:  
-    print(0)  
-else:  
-    for i in switch[1:]:  
-        for j in i:  
-            if lamp[j] - 1 == 0:  
-                break  
-        else:  
-            print(1)  
-            break  
-    else:  
-        print(0)
+    for i in order:
+        lamp[i] += 1
+
+def off_lamp(l, s):
+    for i in s:
+        l[i] -= 1
+
+    return True if 0 not in l else False
+
+
+for s in switch:
+    result = off_lamp(lamp[:], s)
+    if result:
+        print(1)
+        exit()
+print(0)
+
