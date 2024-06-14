26158c31c3808422a1f9c42fac7f64a9a29692b7
jinhyeok <skusus123@gmail.com>
2024-06-14 15:11:16 +0900

    [Silver III] Title: 전공책, Time: 1468 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/16508. 전공책/전공책.py
+++ 백준/Silver/16508. 전공책/전공책.py
@@ -1,50 +1,55 @@
-def get_char_frequency(s):
-    freq = {}
-    for char in s:
-        if char in freq:
-            freq[char] += 1
+import sys
+input = sys.stdin.readline
+
+t = input().rstrip()
+n = int(input())
+
+cost = []
+books = []
+
+min_cost = 1e9
+
+for _ in range(n):
+    c, w = input().rstrip().split()
+    cost.append(int(c))
+    books.append(w)
+
+
+def can_form_target(target, chosen_books):
+    target_freq = {}
+    for char in target:
+        if char in target_freq:
+            target_freq[char] += 1
         else:
-            freq[char] = 1
-    return freq
+            target_freq[char] = 1
 
+    book_freq = {}
+    for idx in chosen_books:
+        for char in books[idx]:
+            if char in book_freq:
+                book_freq[char] += 1
+            else:
+                book_freq[char] = 1
 
-def can_form_target(target_freq, books_freq):
-    for char, count in target_freq.items():
-        total_count = sum(book_freq.get(char, 0) for book_freq in books_freq)
-        if total_count < count:
+    for char in target_freq:
+        if char not in book_freq or book_freq[char] < target_freq[char]:
             return False
+
     return True
 
 
-def find_min_cost(books, target_freq, index=0, current_books=None, min_cost=[float('inf')]):
-    if current_books is None:
-        current_books = []
+def dfs(depth, current_books):
+    global min_cost
 
-    if index == len(books):
-        if can_form_target(target_freq, [get_char_frequency(title) for _, title in current_books]):
-            cost = sum(price for price, _ in current_books)
-            min_cost[0] = min(min_cost[0], cost)
+    if depth == n:
+        if can_form_target(t, current_books):
+            total = sum(cost[idx] for idx in current_books)
+            min_cost = min(total, min_cost)
+
     else:
-        find_min_cost(books, target_freq, index + 1, current_books + [books[index]], min_cost)
-        find_min_cost(books, target_freq, index + 1, current_books, min_cost)
-
-    return min_cost[0]
+        dfs(depth + 1, current_books + [depth])
+        dfs(depth + 1, current_books)
 
 
-t = input().strip()
-n = int(input())
-books = []
-
-for _ in range(n):
-    parts = input().strip().split()
-    price = int(parts[0])
-    title = ''.join(parts[1:])
-    books.append((price, title))
-
-t_freq = get_char_frequency(t)
-
-min_cost = find_min_cost(books, t_freq)
-if min_cost == float('inf'):
-    print(-1)
-else:
-    print(min_cost)
+dfs(0, [])
+print(min_cost if min_cost != 1e9 else -1)
