367d413fc182c9c2cf66b706850fa56984714580
jinhyeok <skusus123@gmail.com>
2024-08-13 09:30:31 +0900

    [level 2] Title: 3 x n 타일링, Time: 1.18 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.py
@@ -0,0 +1,11 @@
+def solution(n):
+    dp = [0] * (n+1)
+    
+    dp[0] = 1
+    dp[2] = 3
+    
+    for i in range(4, n+1):
+        dp[i] = (dp[i-2] * 4 - dp[i-4]) % 1000000007
+        
+        
+    return dp[n] 
