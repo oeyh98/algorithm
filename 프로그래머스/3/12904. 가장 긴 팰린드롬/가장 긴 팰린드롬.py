6bff5c8cd9dc8eb9cad90074ae4378b4efc7805f
jinhyeok <skusus123@gmail.com>
2024-08-13 10:00:11 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 3516.96 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
@@ -0,0 +1,11 @@
+def solution(s):
+    answer = 0
+    is_pal = []
+    for i in range(len(s)):
+        for j in range(len(s), i, -1):
+            if s[i:j] == s[i:j][::-1]:
+                if answer < len(s[i:j]):
+                    answer = len(s[i:j])
+                
+               
+    return answer
