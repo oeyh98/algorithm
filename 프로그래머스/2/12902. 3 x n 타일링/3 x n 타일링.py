2e6e871edeef8cefc31c5fda3e993cca30dcb363
jinhyeok <skusus123@gmail.com>
2024-08-16 15:45:46 +0900

    [level 2] Title: 3 x n 타일링, Time: 268.87 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.py
+++ 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.py
@@ -1,15 +1,11 @@
 def solution(n):
     dp = [0] * (n+1)
+    
     dp[0] = 1
     dp[2] = 3
     
     for i in range(4, n+1, 2):
-        dp[i] = (dp[i-2] * 4 - dp[i-4]) % 1000000007
+        dp[i] = (3 * dp[i-2] + sum([2 * dp[i - j] for j in range(4, i+1, 2)])) % 1000000007
         
-        
-    return dp[n] 
-
-#[0, 0, 3, 0, 11, 0, 61, 0, 153, 571]
-
-
-
+    return dp[n]
+    
