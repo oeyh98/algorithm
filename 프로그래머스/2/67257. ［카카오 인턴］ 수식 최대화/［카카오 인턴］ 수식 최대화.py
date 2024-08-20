9b7305939c3d2b0bc52ba1318b12a54346ecfb9f
jinhyeok <skusus123@gmail.com>
2024-08-20 22:11:18 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.35 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
@@ -1,20 +1,52 @@
-
 from itertools import permutations
+from collections import deque
+
+def operation(left, right, op):
+    left = int(left)
+    right = int(right)
+    
+    if op == '+':
+        return left + right
+    elif op == '-':
+        return left - right
+    else: 
+        return left * right
+
+def calculator(ops, expression):
+    q = deque()
+    temp = ""
+    
+    for ex in expression:
+        if ex.isdigit():
+            temp += ex
+        else:
+            if temp:
+                q.append(temp)
+                temp = ""
+            q.append(ex)
+    
+    if temp:
+        q.append(temp)
+    
+
+    for op in ops:
+        stack = []
+        while q:
+            ex = q.popleft()
+            if ex == op:
+                stack.append(operation(stack.pop(), q.popleft(), op))
+            else:
+                stack.append(ex)
+        q = deque(stack)  # 스택을 다시 큐로 변환
+    
+    return abs(int(q[0]))  # 최종 결과 반환
 
 def solution(expression):
-    operations = ['+', '-', '*']
-    operations = list(permutations(operations, 3))
     answer = []
-    
-    for op in operations:
-        a = op[0]  
-        b = op[1]    
-        temp_list = []
-
-        for ex in expression.split(a):
-            temp = [f"({i})" for i in ex.split(b)]
-            temp_list.append(f'({b.join(temp)})')
-
-        answer.append(abs(eval(a.join(temp_list))))
+    for ops in permutations(['+', '-', '*'], 3):
+        answer.append(calculator(ops, expression))
         
-    return max(answer)  
+    return max(answer)
+
+# 테스트
+print(solution("100-200*300-500+20"))  # 기대값: 60420
