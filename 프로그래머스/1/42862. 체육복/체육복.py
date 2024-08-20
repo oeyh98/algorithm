56841d1bc54427512a539e3b18632510129c6bd4
jinhyeok <skusus123@gmail.com>
2024-08-20 16:21:49 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,27 +1,22 @@
 def solution(n, lost, reserve):
-    dic = {i:1 for i in range(n+1)}
+    dic = {i: 1 for i in range(1, n + 1)}
     answer = 0
-    
+
     for l in lost:
         dic[l] -= 1
-        
+
     for r in reserve:
         dic[r] += 1
-        
-    for idx in dic:
+
+    for idx in range(1, n + 1):
         if dic[idx] == 0:
-            if dic[idx-1] > 1:
-                dic[idx-1] -= 1
+            if idx - 1 > 0 and dic[idx - 1] > 1:
+                dic[idx - 1] -= 1
                 answer += 1
-                
-            elif idx + 1 < n + 1 and dic[idx + 1] > 1:
+            elif idx < n and dic[idx + 1] > 1:
                 dic[idx + 1] -= 1
                 answer += 1
-                
         else:
             answer += 1
-            
-    return answer - 1
-                
-                
-    
+
+    return answer
