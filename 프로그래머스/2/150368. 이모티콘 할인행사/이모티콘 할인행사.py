0e7476c5c5a6855b18ed1c1dcce9e76745e8745d
jinhyeok <skusus123@gmail.com>
2024-08-24 23:07:27 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 3045.94 ms, Memory: 12.1 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
@@ -1,41 +1,38 @@
 def solution(users, emoticons):
-    membership = 0
-    profit = 0
-
-    data = [10, 20, 30, 40]
+    answer = [0, 0]
     discount = []
-
-    def dfs(depth, temp):
+    
+    def bt(depth, temp):
         if depth == len(temp):
             discount.append(temp[:])
             return
-        for d in data:
-            temp[depth] += d
-            dfs(depth + 1, temp)
-            temp[depth] -= d
-
-    dfs(0, [0] * len(emoticons))
-
+            
+        for per in [10, 20, 30, 40]:
+            temp[depth] += per
+            bt(depth + 1, temp)
+            temp[depth] -= per
+            
+    bt(0, [0] * len(emoticons))
+    
     for d in range(len(discount)):
-        join = 0
-        user_fee = [0] * len(users)
-
-        # 할인율 확인, 개인 금액 갱신
-        for e in range(len(emoticons)):
-            for u in range(len(users)):
-                if users[u][0] <= discount[d][e]:
-                    user_fee[u] += int(emoticons[e] / 100 * (100 - discount[d][e]))
-
-        # 멤버쉽 갱신
+        membership = 0
+        profit = 0
         for u in range(len(users)):
-            if users[u][1] <= user_fee[u]:
-                join += 1
-                user_fee[u] = 0
-
-        if membership < join:
-            membership = join
-            profit = sum(user_fee)
-        elif membership == join:
-            profit = max(profit, sum(user_fee))
-
-    return [membership, profit]
+            cost = 0
+            for e in range(len(emoticons)):
+                if users[u][0] <= discount[d][e]:
+                    cost += int(emoticons[e] / 100) * (100 - discount[d][e])
+                if users[u][1] <= cost:
+                    membership += 1
+                    cost = 0
+                    break
+                    
+            profit += cost
+        
+        if answer[0] < membership:
+            answer[0] = membership
+            answer[1] = profit
+        elif answer[0] == membership:
+            answer[1] = max(answer[1], profit)
+            
+    return answer
