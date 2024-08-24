d6585c880fc7b7c6c9334276a5791b2ebc765887
jinhyeok <skusus123@gmail.com>
2024-08-24 20:31:32 +0900

    [level 2] Title: 문자열 압축, Time: 6.84 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -3,24 +3,19 @@ def solution(s):
 
     for i in range(1, len(s)//2 + 1):
         comp_s = ""
-        temp = s[:i]
         cnt = 1
         
-        for j in range(i, len(s), i):
-            if temp == s[j:j+i]:
+        for j in range(0, len(s), i):
+            if s[j:j+i] == s[j+i:j+i*2]:
                 cnt += 1
             else:
                 if 1 < cnt:
                     comp_s += str(cnt)
-                    cnt = 1
-                
-                comp_s += temp    
-                temp = s[j:j+i]
-                
-        if 1 < cnt:
-            comp_s += str(cnt)
-        comp_s += temp
+                    
+                cnt = 1
+                comp_s += s[j:j+i]    
         
         answer = min(answer, len(comp_s)) 
         
     return answer
+
