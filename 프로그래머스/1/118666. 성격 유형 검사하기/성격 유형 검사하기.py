e934cef5c2407ca8e32ba92df4914e3f20429409
jinhyeok <skusus123@gmail.com>
2024-08-28 09:30:59 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.34 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,22 +1,20 @@
+from collections import defaultdict
 def solution(survey, choices):
-    answer = ''
-    dic = {}
-    indicator = ['R', 'T', 'C', 'F', 'J', 'M', 'A', 'N']
-    score = [3, 2, 1, 0, 1, 2, 3]
+    answer = ""
+    indicator = defaultdict(int)
     
-    for i in indicator:
-        dic[i] = 0
+    score = [0, 3, 2, 1, 0, 1, 2, 3]
     
     for i in range(len(survey)):
-        if choices[i] < 4:
-            dic[survey[i][0]]  += score[choices[i]-1]
+        if choices[i] <= 4:
+            indicator[survey[i][0]] += score[choices[i]]
         else:
-            dic[survey[i][1]]  += score[choices[i]-1]
-            
-    answer += 'T' if dic['R'] < dic['T'] else 'R'
-    answer += 'F' if dic['C'] < dic['F'] else 'C'
-    answer += 'M' if dic['J'] < dic['M'] else 'J'
-    answer += 'N' if dic['A'] < dic['N'] else 'A'
+            indicator[survey[i][1]] += score[choices[i]]
     
-    
+    print(indicator)
+    answer += "R" if indicator["R"] >= indicator["T"] else "T"
+    answer += "C" if indicator["C"] >= indicator["F"] else "F"
+    answer += "J" if indicator["J"] >= indicator["M"] else "M"
+    answer += "A" if indicator["A"] >= indicator["N"] else "N"
+        
     return answer
