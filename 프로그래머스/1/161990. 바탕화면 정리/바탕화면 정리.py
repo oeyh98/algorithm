1aaf6085ff578de7559c9e5619ccf9c6aafeb8ec
jinhyeok <skusus123@gmail.com>
2024-08-30 09:21:42 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.68 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -1,18 +1,11 @@
 def solution(wallpaper):
-    lux, luy = 51, 51
-    rdx, rdy = 0, 0
+    x, y = [], []
     
     for i in range(len(wallpaper)):
         for j in range(len(wallpaper[0])):
             if wallpaper[i][j] == '#':
-                if i < lux:
-                    lux = i
-                if j < luy:
-                    luy = j
-                if rdx < i + 1:
-                    rdx = i + 1
-                if rdy < j + 1:
-                    rdy = j + 1
+                x.append(i)
+                y.append(j)
         
     
-    return [lux, luy, rdx, rdy]
+    return [min(x), min(y), max(x) + 1, max(y) + 1]
