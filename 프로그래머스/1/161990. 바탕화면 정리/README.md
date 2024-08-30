9e48f1a8c5bad5d53930579563248c06ca524235
jinhyeok <skusus123@gmail.com>
2024-08-30 09:18:25 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.26 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/README.md
+++ 프로그래머스/1/161990. 바탕화면 정리/README.md
@@ -1,10 +1,10 @@
 # [level 1] 바탕화면 정리 - 161990 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161990?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161990?language=python3) 
 
 ### 성능 요약
 
-메모리: 78.2 MB, 시간: 0.23 ms
+메모리: 10.1 MB, 시간: 0.26 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 12일 11:02:06
+2024년 08월 30일 09:18:22
 
 ### 문제 설명
 
--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -5,13 +5,14 @@ def solution(wallpaper):
     for i in range(len(wallpaper)):
         for j in range(len(wallpaper[0])):
             if wallpaper[i][j] == '#':
-                if lux > i:
+                if i < lux:
                     lux = i
-                if luy > j:
+                if j < luy:
                     luy = j
-                if rdx < i:
-                    rdx = i
-                if rdy < j:
-                    rdy = j
+                if rdx < i + 1:
+                    rdx = i + 1
+                if rdy < j + 1:
+                    rdy = j + 1
+        
     
-    return [lux, luy, rdx + 1, rdy + 1]
+    return [lux, luy, rdx, rdy]
