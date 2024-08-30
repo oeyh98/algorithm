7a6572b9f27c50258a382d80da2f5b25ca80e7c7
jinhyeok <skusus123@gmail.com>
2024-08-30 14:50:06 +0900

    [level 3] Title: 순위, Time: 7.33 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.py
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -1,21 +1,21 @@
 from collections import defaultdict
-
 def solution(n, results):
     answer = 0
-    win = defaultdict(set)
-    lose = defaultdict(set)
+    victory = defaultdict(set)
+    defeat = defaultdict(set)
     
-    for a, b in results:
-        win[a].add(b)
-        lose[b].add(a)
+    for win, lose in results:
+        victory[win].add(lose)
+        defeat[lose].add(win)
         
     for i in range(1, n+1):
-        for defeat in win[i]:
-            lose[defeat].update(lose[i])
-        for victory in lose[i]:
-            win[victory].update(win[i])
-        
+        for loser in victory[i]:
+            defeat[loser].update(defeat[i])
+        for winner in defeat[i]:
+            victory[winner].update(victory[i])
+            
     for i in range(1, n+1):
-        if len(win[i]) + len(lose[i]) == n-1:
+        if len(victory[i] | defeat[i]) == n - 1:
             answer += 1
+            
     return answer
