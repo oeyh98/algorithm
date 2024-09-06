2350ecdc8eff53d36dd3ae36d96f496e5595d0d1
jinhyeok <skusus123@gmail.com>
2024-09-06 16:04:08 +0900

    [level 3] Title: 단어 변환, Time: 0.34 ms, Memory: 70.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class Solution {
+    boolean[] visited;
+    int answer = 0;
+    void dfs(String begin, String target, String[] words, int cnt) {
+        if(begin.equals(target)){
+            answer = cnt;
+            return;
+        }
+        
+        for(int i = 0; i < words.length; i++){
+            int diff = 0;
+            
+            if(visited[i] == true){
+                continue;
+            }
+            
+            for(int j = 0; j < begin.length(); j++){
+                
+                if(begin.charAt(j) != words[i].charAt(j)){
+                    diff++;
+                }
+            }
+            
+            if(diff == 1){
+                visited[i] = true;
+                dfs(words[i], target, words, cnt+1);
+                visited[i] = false;
+            }   
+        }
+    }
+    public int solution(String begin, String target, String[] words) { 
+        visited = new boolean[words.length];
+        dfs(begin, target, words, 0);
+        return answer;
+    }
+}
