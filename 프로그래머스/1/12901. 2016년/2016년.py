658b5d161fddf832f49babfab0b3172333198ebb
jinhyeok <skusus123@gmail.com>
2024-09-26 10:36:16 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.py
+++ 프로그래머스/1/12901. 2016년/2016년.py
@@ -1,11 +1,11 @@
 def solution(a, b):
-    week = ["FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"]
+    week = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"]
     month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     
     temp = 0
     for i in range(a-1):
         temp += month[i]
         
-    day = week[(b + temp -1) % 7]
+    day = week[(b + temp) % 7]
 
     return day
